3daf1094b4cc635540d5a30d22c638e07158209f
subedisulav850-a11y <subedisulav850@gmail.com>
2026-01-21 19:55:13 +0545

    Add files via upload

---
+++ inline_scripts.js
@@ -0,0 +1,14 @@
+
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag("js", new Date());
+    gtag("config", "G-55F3J8VB7D");
+  
+
+/* ===== INLINE JAVASCRIPT ===== */
+
+document.write( unescape( '%3C%6C%69%6E%6B%20%72%65%6C%3D%22%73%74%79%6C%65%73%68%65%65%74%22%20%68%72%65%66%3D%22%2F%49%74%65%6D%73%2F%73%74%79%6C%65%2E%63%73%73%22%20%2F%3E' ))
+
+/* ===== INLINE JAVASCRIPT ===== */
+
+document.write( unescape( '%3C%73%63%72%69%70%74%20%73%72%63%3D%22%2F%49%74%65%6D%73%2F%73%63%72%69%70%74%2E%6A%73%22%3E%3C%2F%73%63%72%69%70%74%3E' ))
